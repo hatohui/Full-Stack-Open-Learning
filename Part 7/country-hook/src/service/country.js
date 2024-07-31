198244f80193f36a3360147bb84212aaaab1b36f
Lê Sỹ Tuyền <130513663+Hatohui@users.noreply.github.com>
2024-08-01 00:38:58 +0700

    yes

--- Part 7/country-hook/src/service/country.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/name/";
-
-export const fetchByName = async (name) => {
-  const response = await axios.get(`${baseUrl}${name}`);
-  return response.data;
-};
--- Part 7/routed-anecdotes/src/components/Notification.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/prop-types */
-const Notification = ({ notification }) => (
-  <>{notification !== "" ? <p>{notification}</p> : null}</>
-);
-
-export default Notification;
